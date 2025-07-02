a1478883d7188765e85c1d7d9266a271b3bc3d41
Neeraj Kumar Yogi <nickyogi2143@gmail.com>
2025-07-02 18:39:59 +0530

    Bugs Fixed

--- backend/config/challengeCron.js
+++ backend/config/challengeCron.js
@@ -1,7 +1,7 @@
 import cron from "node-cron";
 import { checkAndFailChallenges } from "../controllers/challengeContrller.js";
 
-// checkAndFailChallenges();
+checkAndFailChallenges();
 
 // Schedule daily check at 1:00 AM
 cron.schedule("0 1 * * *", async () => {
